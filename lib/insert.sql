ffb32fd7beeda81721cddbd3e7d5296af67aed59
Daniella Rono <daniellarono@gmail.com>
2025-02-07 18:49:38 +0300

    all tests are passing

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+    ('Mr. Chocolate', 20, 'M', 'Brown', 'Calm', 0),
+    ('Rowdy', 10, 'M', 'Black', 'Aggressive', 1),
+    ('Tabitha', 6, 'F', 'Golden', 'Friendly', 1),
+    ('Sergeant Brown', 10, 'M', 'Brown', 'Stoic', 0),
+    ('Melissa', 13, 'F', 'Black', 'Gentle', 1),
+    ('Grinch', 2, 'M', 'White', 'Grumpy', 1),
+    ('Wendy', 6, 'F', 'Golden', 'Playful', 1),
+    (NULL, 15, 'M', 'Gray', 'Mysterious', 1);
